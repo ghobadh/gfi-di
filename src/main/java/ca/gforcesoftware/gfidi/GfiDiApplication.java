2747ebe7ab33d6f06d2732febe77df728c2c9ca1
Gavin Hashemi <Gavin.Hashemi@gforcesoftware.ca>
2023-09-13 18:34:56 -0400

    Initial Project Commit to Git

---
+++ src/main/java/ca/gforcesoftware/gfidi/GfiDiApplication.java
@@ -0,0 +1,13 @@
+package ca.gforcesoftware.gfidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfiDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfiDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/gforcesoftware/gfidi/GfiDiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gforcesoftware.gfidi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfiDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
